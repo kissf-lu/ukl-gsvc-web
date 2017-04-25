ac92c8fe15cad54020ff2f114920d81f1edf5c4a
kissf <chois_lu@outlook.com>
2017-04-25 09:44:02 +0800

    添加套餐流量查询接口

--- app/api_1_0/__init__.py
+++ app/api_1_0/__init__.py
@@ -3,7 +3,9 @@ from flask import Blueprint
 
 api = Blueprint('api', __name__)
 
-from . import authentication, users, errors, country, mutiCountry,exportFilesAPI,webModifySqlAPI
+from . import (authentication, users, errors, country,
+               mutiCountry,exportFilesAPI,webModifySqlAPI,
+               )
 from api_functions import exportExcelFunc
 
 
